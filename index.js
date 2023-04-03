b0d9811ec827ee08766fd89a13266758a411da47
JrSchmidtt <junior.schmidt.99@gmail.com>
2023-04-03 13:53:42 -0300

    json file

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const results = [];
+
+fs.createReadStream('iata-icao.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        fs.writeFile('result.json', JSON.stringify(results), (err) => {
+            if (err) {
+                console.log('Erro ao escrever arquivo JSON: ', err);
+            } else {
+                console.log('Arquivo JSON criado com sucesso!');
+            }
+        });
+    });
